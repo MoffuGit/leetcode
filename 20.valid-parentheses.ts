0a5352de6d415a2be2e19399f4eb4389e0cf9d3d
MoffuGit <moffu.by@gmail.com>
2025-09-02 10:32:46 -0600

    valid parentheses

--- 20.valid-parentheses.ts
+++ 20.valid-parentheses.ts
@@ -1,5 +1,29 @@
 // @leet start
 function isValid(s: string): boolean {
+    let map = new Map([
+        [')', '('],
+        ['}', '{'],
+        [']', '[']
+    ])
     let stack: string[] = [];
+
+    for (let char of s) {
+        let valid = map.get(char)
+        if (valid === undefined) {
+            stack.push(char)
+            continue;
+        }
+
+        let poped = stack.pop();
+        if (poped === undefined) {
+            return false;
+        }
+
+        if (valid !== poped) {
+            return false
+        }
+    }
+
+    return stack.length === 0;
 };
 // @leet end
