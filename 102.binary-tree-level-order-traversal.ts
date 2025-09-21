59d72563033b0ed7782f00edd459dd4a4447047f
MoffuGit <moffu.by@gmail.com>
2025-09-20 19:43:13 -0600

    BinaryTreeLevelOrderTraversal

---
+++ 102.binary-tree-level-order-traversal.ts
@@ -0,0 +1,37 @@
+// @leet start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function levelOrder(root: TreeNode | null): number[][] {
+    let res: number[][] = []
+
+    function dfs(node: TreeNode | null, depth: number): void {
+        if (!node) {
+            return
+        }
+
+        if (res.length === depth) {
+            res.push([]);
+        }
+
+        res[depth].push(node.val);
+        dfs(node.left, depth + 1);
+        dfs(node.right, depth + 1);
+    }
+
+    dfs(root, 0);
+
+    return res
+};
+// @leet end
