12b201e1e6bb9fb24e07165d72b6b818724da55a
MoffuGit <moffu.by@gmail.com>
2025-09-21 21:03:52 -0600

    ValidateBinarySearchTree

---
+++ 98.validate-binary-search-tree.ts
@@ -0,0 +1,31 @@
+// @leet start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isValidBST(root: TreeNode | null): boolean {
+    return valid(root, -Infinity, Infinity)
+};
+
+function valid(node: TreeNode | null, left: number, right: number): boolean {
+    if (!node) {
+        return true
+    }
+
+    if (!(left < node.val && node.val < right)) {
+        return false
+    }
+
+    return valid(node.left, left, node.val) && valid(node.right, node.val, right)
+}
+// @leet end
